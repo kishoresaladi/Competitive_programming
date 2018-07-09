c8bd779173d115baab027a32d2804343c3f25688
saladikishore <saladikishore01@gmail.com>
2018-07-09 17:57:25 +0530

    Added Week_03 Day_01

---
+++ week_3/Day 1/7sided_die.py
@@ -0,0 +1,23 @@
+import random
+
+
+def rand5():
+    return random.randint(1, 5)
+
+
+def random7():
+    return 5 * rand5() + rand5() - 5
+def rand7():
+
+    # Implement rand7() using rand5()
+    while True:
+        r = random7()
+        if r < 22:
+            return (r%7+1)
+
+
+    return 0
+
+
+print 'Rolling 7-sided die...'
+print rand7()
