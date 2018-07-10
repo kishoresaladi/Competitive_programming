13bf7b7d1cc4f116196aebff7e079e662ee0af59
saladikishore <saladikishore01@gmail.com>
2018-07-10 17:33:15 +0530

    added day-02

--- week_3/Day 2/eggs.py
+++ week_3/Day 2/eggs.py
@@ -4,14 +4,14 @@ def my_function(arg):
     k=14
     y=0
     while(y<x):
-    		z=y
-    		y=y+k
-    		k=k-1
-    		count=count+1
+            z=y
+            y=y+k
+            k=k-1
+            count=count+1
     if(x==y):
         return count
     count=count+(x-z)
     return count
 
-    
-print(my_function(13))
+for i in range(1,100):
+    print(my_function(i))
